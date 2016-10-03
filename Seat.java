c3ec296a334202566061eaba4190c855e42e31a4
Karan Bhargava <karan1525@yahoo.co.in>
2016-10-03 11:07:41 -0700

    Final Commit
    Commit to gitHub on assignment completion

---
+++ Seat.java
@@ -0,0 +1,93 @@
+/**
+ * A class representing seats in the plane
+ * @author Karan Bhargava
+ * @version 1.5718
+ * 
+ */
+public class Seat {
+	
+	private String seatType; //type
+	private String seatNumber; //number
+	private Passenger passenger;
+	
+	/**
+	 * A constructor to build the seats for the plane
+	 * Includes specific instructions on how the plane is set
+	 * @param number - the seat number
+	 * @param classPref - the class in which the seat is
+	 */
+	public Seat (String number, String classPref) {
+		
+		passenger = null; //every seat is null until a passenger is seated
+		this.seatNumber = number; //set the seat number
+		
+		if (classPref.equalsIgnoreCase("E")) {
+			
+			if (number.contains ("D") || number.contains("C")) {
+				
+					this.seatType = "A";
+					
+			} else if (number.contains("A") || number.contains("F")) {
+				
+					this.seatType = "W";
+					
+			} else  {
+				
+					this.seatType = "C";
+					
+			}
+		} else if (classPref.equalsIgnoreCase("F")) {
+				if (number.contains("D") || number.contains("A")) {
+					
+					this.seatType = "W";
+					
+				} else {
+					
+					this.seatType = "A";
+					
+				}
+		}
+		
+	}
+
+	/**
+	 * A getter method to get the seat number
+	 * @return seatNumber - the seat number 
+	 */
+	public String getSeatNumber() {
+		
+		return seatNumber;
+		
+	}
+	
+	/**
+	 * A getter method to get the seat type
+	 * @return seatType -  (Window, Aisle or Center)
+	 */
+	public String getSeatType() {
+		
+		return seatType;
+		
+	}
+	
+	/**
+	 * A getter method to get the passenger assigned to a seat
+	 * @return passenger - the passenger assigned
+	 */
+	public Passenger getPassenger() {
+		
+		return passenger;
+		
+	}
+	
+	/**
+	 * A setter method to set a passenger to a seat
+	 * @param occupant the passenger to set
+	 */
+	public void setPassenger(Passenger occupant) {
+		
+		passenger = occupant;
+		
+	}
+	
+}
